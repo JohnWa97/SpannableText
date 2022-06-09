7c45583d11949d18e284d4d9e47e9a83ef3b3ba0
丘祖桦 <qiuzuhua@hudunsoft.com>
2022-06-09 10:02:52 +0800

    V1.0.0 创建代码

---
+++ SpannableText/src/main/java/com/johnwa/spannabletext/Calculate.java
@@ -0,0 +1,13 @@
+package com.johnwa.spannabletext;
+
+/**
+ * @Author: qzh
+ * @Date: 2022/6/9
+ * @Description:
+ */
+public class Calculate {
+
+    public int sum(int num1, int num2){
+        return num1 + num2;
+    }
+}
